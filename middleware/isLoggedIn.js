7f3b844228a0afd0a3ff9e458d92dbce1263c21a
Anthony Gregis <anthony@thedevtron.com>
2020-08-19 12:45:18 -0500

    [Web Server] Testing

--- middleware/isLoggedIn.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    if (!req.user) {
-        req.flash('Error', 'You must be signed to access this page')
-        res.redirect('/auth/login')
-    } else {
-        next()
-    }
-}
